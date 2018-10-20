8208f1ed060fd50b051e339f93df3fc9da63c272
xingxing <17875742221@163.com>
2018-10-20 08:54:31 +0800

    新增组织等文件 2018-10-20 88:54

---
+++ pages/addplan/addplan.js
@@ -0,0 +1,15 @@
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    date: '',
+  },
+
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+})
---
+++ pages/allocationscore/allocationscore.js
@@ -0,0 +1,22 @@
+//manageorg.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    yearList: [
+      {
+        'year':'2018',
+        'monthList' : ['十一月','九月','七月']
+      },
+      {
+        'year': '2017',
+        'monthList': ['十一月', '九月', '七月']
+      },
+      {
+        'year': '2016',
+        'monthList': ['十一月', '九月', '七月']
+      },
+    ]
+  },
+})
